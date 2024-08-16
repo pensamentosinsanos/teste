9050d104e77135e208009c5e4f8425d8e5c07e93
Vitoria Carlosso <84900172049@senacrs.edu.br>
2024-08-16 11:13:27 -0300

    aprendendojavascript

---
+++ script.js
@@ -0,0 +1,9 @@
+//perguntar ao usuario
+let nome=prompt("Digite seu Nome:");
+alert("Ola "+nome+" Tudo Bem?");
+
+//alerta na tela
+alert("Ola Mundo");
+//console do navegador
+console.log("Teste de Console");
+document.getElementById("demo").innerHTML= "Poggers";
